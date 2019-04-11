ad79688edd2f6eb55a9cfd9f4b6cdec02e2750b7
Kevin Scardina <kevin.scardina@davita.com>
2019-04-10 18:05:33 -0600

    adding variable fonts

--- Fonts/Fonts/ViewController.swift
+++ Fonts/Fonts/ViewController.swift
@@ -9,37 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var isCustom = false
+    
     let allTextStyles: [(UIFont.TextStyle, String)] = [(.body, "Body"), (.callout, "Callout"), (.caption1, "Caption 1"),
                                                        (.caption2, "Caption 2"), (.footnote, "Footnote"), (.headline, "Headline"),
                                                        (.largeTitle, "Large Title"), (.subheadline, "Subheadline"),
                                                        (.title1, "Title 1"), (.title2, "Title 2"), (.title3, "Title 3")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        refreshView()
+    }
+    
+    @IBAction func switchChanged(_ sender: Any?) {
+        guard let swtch = sender as? UISwitch else {
+            return
+        }
+        
+        isCustom = swtch.isOn
+        
+        refreshView()
+    }
+    
+    private func refreshView() {
+        view.subviews.forEach { $0.removeFromSuperview() }
+        
+        let swtch = UISwitch()
+        swtch.isOn = isCustom
+        swtch.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(swtch)
+        swtch.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        swtch.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor).isActive = true
+        swtch.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
+        
         let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
-        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: swtch.bottomAnchor, constant: 8).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
         scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        
         var previousLabel: UILabel?
         allTextStyles.forEach({ (tuple) in
             let label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
+            label.adjustsFontForContentSizeCategory = true
             label.numberOfLines = 0
-            label.font = UIFont.preferredFont(forTextStyle: tuple.0)
+            var font = UIFont.preferredFont(forTextStyle: tuple.0)
+            if isCustom, let customFont = UIFont(name: "Papyrus", size: UIFont.labelFontSize) {
+                font = customFont
+            }
+            label.font = UIFontMetrics(forTextStyle: tuple.0).scaledFont(for: font)
             label.text = tuple.1
             scrollView.addSubview(label)
-            label.topAnchor.constraint(equalTo: previousLabel == nil ? scrollView.layoutMarginsGuide.topAnchor : previousLabel!.layoutMarginsGuide.bottomAnchor, constant: 18).isActive = true
-            label.leftAnchor.constraint(equalTo: scrollView.layoutMarginsGuide.leftAnchor).isActive = true
-            label.rightAnchor.constraint(equalTo: scrollView.layoutMarginsGuide.rightAnchor).isActive = true
+            label.topAnchor.constraint(equalTo: previousLabel == nil ? scrollView.topAnchor : previousLabel!.layoutMarginsGuide.bottomAnchor, constant: 18).isActive = true
+            label.leftAnchor.constraint(equalTo: scrollView.leftAnchor).isActive = true
+            label.rightAnchor.constraint(equalTo: scrollView.rightAnchor).isActive = true
             previousLabel = label
         })
+        if let label = previousLabel {
+            label.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        }
     }
-
-
 }
 
